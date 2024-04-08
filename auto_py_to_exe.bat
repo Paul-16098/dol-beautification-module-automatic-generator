350efce4b7aa45f419c89b4f725e52d702fe08c3
Paul-16098 <pl816098@gmail.com>
2024-04-08 18:11:44 +0800

    modified:   .github/workflows/python-app.yml 	modified:   auto_py_to_exe.bat 	renamed:    "output/dol\347\276\216\345\214\226\346\250\241\347\273\204\350\207\252\345\212\250\347\224\237\346\210\220\345\231\250.exe" -> "dist/dol\347\276\216\345\214\226\346\250\241\347\273\204\350\207\252\345\212\250\347\224\237\346\210\220\345\231\250.exe" 	modified:   "dol\347\276\216\345\214\226\346\250\241\347\273\204\350\207\252\345\212\250\347\224\237\346\210\220\345\231\250.exe"

--- auto_py_to_exe.bat
+++ auto_py_to_exe.bat
@@ -3,9 +3,9 @@ cls
 set "py_path=%1"
 set "script_dir=%~dp0"
 if "%py_path%"=="" (
-    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%output" -c "default_cofg.json"
+    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%dist" -c "default_cofg.json"
 ) else (
-    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%output" -c "default_cofg.json" "%py_path%"
+    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%dist" -c "default_cofg.json" "%py_path%"
 )
 @REM pause
 exit 0
