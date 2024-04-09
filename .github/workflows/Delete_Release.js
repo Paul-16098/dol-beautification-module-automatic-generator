4055303cc58f5e09f7ad66fb765082019a938667
Paul-16098 <pl816098@gmail.com>
2024-04-09 12:56:05 +0800

    new file:   .github/workflows/Delete_Release.js 	modified:   .github/workflows/auto-py-to-exe.yml

---
+++ .github/workflows/Delete_Release.js
@@ -0,0 +1,28 @@
+import { getOctokit } from "@actions/github";
+
+const deleteRelease = async () => {
+  try {
+    const octokit = getOctokit("${{ secrets.TOKEN }}");
+
+    const releases = await octokit.rest.repos.listReleases({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+
+    const release = releases.data.find((r) => r.tag_name === "last-Release");
+    if (release) {
+      await octokit.rest.repos.deleteRelease({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        release_id: release.id,
+      });
+      console.log("Release deleted successfully.");
+    } else {
+      console.log("Release not found.");
+    }
+  } catch (error) {
+    console.error("Error deleting release:", error);
+  }
+};
+
+deleteRelease();
