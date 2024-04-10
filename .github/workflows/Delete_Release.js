69c0d4bde9ff083f7d6ab347c254572387d9c493
Paul-16098 <pl816098@gmail.com>
2024-04-10 12:52:13 +0800

    modified:   .github/workflows/Delete_Release.js

--- .github/workflows/Delete_Release.js
+++ .github/workflows/Delete_Release.js
@@ -1,23 +1,27 @@
-const { getOctokit, context } = require('@actions/github');
+const { getOctokit, context } = require("@actions/github");
 
-// Octokit.js
-// https://github.com/octokit/core.js#readme
-const octokit = new Octokit({
-  auth: "${{ secrets.TOKEN }}"
-})
+async function deleteRelease() {
+  const octokit = new Octokit({
+    auth: "${{ secrets.TOKEN }}",
+  });
 
-const releases = await octokit.rest.repos.listReleases({
+  const releases = await octokit.rest.repos.listReleases({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+  const release = releases.data.find((r) => r.tag_name === "last-Release");
+
+  await octokit.request(
+    "DELETE /repos/Paul-16098/dol-beautification-module-automatic-generator/releases/{release_id}",
+    {
       owner: context.repo.owner,
       repo: context.repo.repo,
-    });
-const release = releases.data.find((r) => r.tag_name === "last-Release");
+      release_id: release.id,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+}
 
-
-await octokit.request('DELETE /repos/Paul-16098/dol-beautification-module-automatic-generator/releases/{release_id}', {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        release_id: release.id,
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})
+deleteRelease();
