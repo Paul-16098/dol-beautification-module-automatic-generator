a740a5c7040fad7ee514f652024b6b5bdcb7898d
Paul-16098 <pl816098@gmail.com>
2024-04-10 18:30:13 +0800

    deleted:    .github/workflows/Delete_Release.bat 	modified:   .github/workflows/auto-py-to-exe.yml

--- .github/workflows/Delete_Release.bat
+++
@@ -1,4 +0,0 @@
-set TOKEN=%TOKEN%
-echo %TOKEN%
-
-curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer %TOKEN%" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/Paul-16098/dol-beautification-module-automatic-generator/releases/tags/last-Release>output.json
