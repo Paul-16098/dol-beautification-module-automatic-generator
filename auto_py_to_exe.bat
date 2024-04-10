c69cd19cd0067b10d4b9973614e7c39355c097dd
Paul-16098 <pl816098@gmail.com>
2024-04-10 21:32:41 +0800

    modified:   auto_py_to_exe.bat 	modified:   "dist/dol\347\276\216\345\214\226\346\250\241\347\273\204\350\207\252\345\212\250\347\224\237\346\210\220\345\231\250.exe" 	modified:   "dol\347\276\216\345\214\226\346\250\241\347\273\204\350\207\252\345\212\250\347\224\237\346\210\220\345\231\250.exe"

--- auto_py_to_exe.bat
+++ auto_py_to_exe.bat
@@ -6,6 +6,7 @@ if "%py_path%"=="" (
     py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%dist" -c "default_cofg.json"
 ) else (
     py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%dist" -c "default_cofg.json" "%py_path%"
+    copy /v /y dist\dol美化模组自动生成器.exe .
 )
 @REM pause
 exit 0
